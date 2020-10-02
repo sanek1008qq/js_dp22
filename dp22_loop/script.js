d6b9970dbd7adac62c3a4c493398b411cc40d6bb
[Students] Aleksandrs Ciula <92DPACiula@rvt.lv>
2020-10-02 14:41:21 +0300

    0.6_new

---
+++ dp22_loop/script.js
@@ -0,0 +1,8 @@
+console.log('QQ')
+
+function showNumbers(totalRows){
+
+    for
+
+
+}
--- script.js
+++ script.js
@@ -1,10 +1,1 @@
 console.log('QQ')
-
-let mainigais1 = 'Viens'
-let mainigais2 = 'Divi'
-
-console.log('mainigais1: ', mainigais1)
-console.log('mainigais2: ', mainigais2)
-
-console.log('mainigais1: ', mainigais2)
-console.log('mainigais2: ', mainigais1)
