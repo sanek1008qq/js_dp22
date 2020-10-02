15f0a9a0dd33e0bcb88883f743a7f25d23dc1286
[Students] Aleksandrs Ciula <92DPACiula@rvt.lv>
2020-10-02 14:53:11 +0300

    0.8.1_script update

---
+++ 03_array/script.js
@@ -0,0 +1,12 @@
+console.log('Array!')
+
+
+function generateArray(start, end){
+
+    let rangeArray = []
+
+    for(let i = start; 1 <= end; 1++){
+        rangeArray.push(1)
+    }
+    console.log(rangeArray)
+}
