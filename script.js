58b2a5f546dc09059bb9b1e8bcf8e025f849ed49
[Students] Aleksandrs Ciula <92DPACiula@rvt.lv>
2020-10-02 14:33:47 +0300

    0.6

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ console.log('QQ')
 let mainigais1 = 'Viens'
 let mainigais2 = 'Divi'
 
-console.log('mainigais1', mainigais1)
-console.log('mainigais2', mainigais2)
+console.log('mainigais1: ', mainigais1)
+console.log('mainigais2: ', mainigais2)
+
+console.log('mainigais1: ', mainigais2)
+console.log('mainigais2: ', mainigais1)
