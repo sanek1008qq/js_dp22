2a2f75d9dd89cf296af5a595849e95cbd315f130
[Students] Aleksandrs Ciula <92DPACiula@rvt.lv>
2020-10-02 15:31:44 +0300

    1.0

--- 05_uzd/script.js
+++ 05_uzd/script.js
@@ -1,4 +1,26 @@
 console.log('uzd!')
 
 
-function 
+function biggestNumber(a, b, c){
+    if(a > b & a > c){
+        if(b < a & b > c){
+            console.log(a, b, c)
+        }else{
+            console.log(a, c, b)
+        }
+    }
+    if(b > a & b > c){
+        if(a < b & a > c){
+            console.log(b, a, c)
+        }else{
+            console.log(b, c, a)
+        }
+    }
+    if(c > a & c > b){
+        if(a < c & a > b){
+            console.log(c, a, b)
+        }else{
+            console.log(c, b, a)
+        }
+    }
+}
